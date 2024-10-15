a0497a55d7e961efd356304e401cfb042001514a
Siddhartha Gudipati <siddu.siddhartha@gmail.com>
2024-10-15 08:43:39 -0700

    feat(sitemap): update blog post retrieval to handle asynchronous data and correct property access

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,9 +3,15 @@ import { getBlogPosts } from "app/blog/utils";
 export const baseUrl = "https://portfolio-blog-starter.vercel.app";
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
+  let blogs = await getBlogPosts();
+
+  if (!blogs) {
+    return [];
+  }
+
+  blogs.map((post) => ({
+    url: `${baseUrl}/blog/${post.frontmatter.slug}`,
+    lastModified: post.frontmatter.publishedAt,
   }));
 
   let routes = ["", "/blog"].map((route) => ({
